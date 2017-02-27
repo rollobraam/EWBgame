a7fd7db4f81ed3a79d29256e84853a5f591322b1
Dylan Braam <dybraam@gmail.com>
2017-02-26 17:36:49 -0800

    Begin adding events
    **None of these events have been tested yet**
    
    Add midterm event
    Add guest speaker event
    Add national conference event

--- A Dark Room Commented/script/events/global.js
+++ A Dark Room Commented/script/events/global.js
@@ -2,6 +2,172 @@
  * Events that can occur when any module is active (Except World. It's special.)
  **/
 Events.Global = [
+	
+	{ /* Midterms */
+		title: _('Midterms'),
+		isAvailable: fuction() {
+			return $SM.get('game.population') >= 20
+		},
+		scenes: {
+			'start': {
+				text: [
+					_('a lot of members have midterms this week')
+				],
+				notification: _('midterm season'),
+				blink: true,
+				buttons: {
+					'study': {
+						text: _('let them study'),
+						nextScene: 'study'
+					}
+				}
+			},
+			'study': {
+				text: [
+					_('school comes first.')
+				],
+				onLoad: function() {
+					$SM.set('game.visibility', $SM.get('game.visibility') - 10)
+					if ($SM.get('game.population') > $SM.get('game.visibility')) {
+						room.updatePopulation()
+					}
+				},
+				buttons: {
+					'close': {
+						text: _('close'),
+						nextScene: 'end'
+					}
+				}
+			}
+		}
+	},
+	{ /* Guest Speaker */
+		title: _('Guest Speaker'),
+		isAvailable: true,
+		scenes: {
+			'start': {
+				text: [
+					_('a notable speaker comes to give a lecture')
+				],
+				notification: _('guest speaker'),
+				blink: true,
+				buttons: {
+					'host': {
+						cost: {'funds': 300}
+						text: _('host the event'),
+						nextScene: {0.7: 'host good', 1: 'host bad'}
+					},
+					'another time': {
+						text: _('another time'),
+						nextScene: 'end'
+					}
+				}
+			},
+			'host good': {
+				text: [
+					_('attendees clamour to ask questions')
+				],
+				onLoad: function() {
+					$SM.set('game.population', $SM.get('game.population') + 6)
+				},
+				buttons: {
+					'close': {
+						text: _('close'),
+						nextScene: 'end'
+					}
+				}
+			},
+			'host bad': {
+				text: [
+					_('the event is poorly timed')
+				],
+				buttons: {
+					'close': {
+						text: _('close'),
+						nextScene: 'end'
+					}
+				}
+			}
+		}
+	}
+	
+	{ /* National Conference */
+		title: _('National Conference'),
+		isAvailable: function() {
+			return $SM.get('game.population') >= 15
+		}
+		scenes: {
+			'start': {
+				text: [
+					_('the annual EWB national conference is being held in Toronto')
+				],
+				notification: _('national conference').
+				blink: true,
+				buttons: {
+					'send five': {
+						cost: {'funds': 500},
+						text: _('send five person delegation'),
+						nextScene: 'send five'
+					},
+					'send fifteen': {
+						cost: {'funds': 1500},
+						text: _('send fifteen person delegation'),
+						nextScene: 'send fifteen'
+					},
+					'send zero': {
+						text: _('send nobody'),
+						nextScene: 'send nobody'
+					}
+				}
+			},
+			'send five': {
+				text: {
+					_('countless workshops and late night discussions in the hotel lobby')
+				},
+				onLoad: function() {
+					$SM.set('game.population', $SM.get('game.population') + 5)
+				},
+				buttons: {
+					'close': {
+						text: _('close'),
+						nextScene: 'end'
+					}
+				}
+			},
+			'send fifteen': {
+				text: {
+					_('chapter bonding in a tiny downtown restaurant')
+				},
+				onLoad: function() {
+					$SM.set('game.population', $SM.get('game.population') + 15)
+				},
+				buttons: {
+					'close': {
+						text: _('close'),
+						nextScene: 'end'
+					}
+				}
+			},
+			'send zero': {
+				text: {
+					_('some discontent on a missed opportunity')
+				},
+				onLoad: function() {
+					$SM.set('game.visibility', $SM.get('game.visibility') - 5)
+					if ($SM.get('game.population') > $SM.get('game.visibility')) {
+						room.updatePopulation()
+					}
+				},
+				buttons: {
+					'close': {
+						text: _('close'),
+						nextScene: 'end'
+					}
+				}
+			}
+		}
+	}
+	
 	{ /* The Thief */
 		title: _('The Thief'),
 		isAvailable: function() {
